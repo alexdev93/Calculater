6aeba06e49d3f8e30a47fbe703f41761a35e4da0
alemayehu.dev <alemayehu.dev@gmail.com>
2025-05-10 19:45:24 +0300

    make dry and bind with keyboard

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const calc = new Calculator(
   document.querySelector(".previousOperand")
 );
 
+// === Utility: Add click listeners in a DRY way ===
 const addClick = (selector, callback) => {
   document
     .querySelectorAll(selector)
@@ -14,11 +15,42 @@ const addClick = (selector, callback) => {
     );
 };
 
-addClick(".number", (num) => calc.appendNumber(num));
-addClick(".operation", (op) => calc.chooseOperator(op));
+// === Click Bindings ===
+addClick(".number", calc.appendNumber.bind(calc));
+addClick(".operation", calc.chooseOperator.bind(calc));
 
 document
   .querySelector("[equal]")
   .addEventListener("click", () => calc.compute());
 document.querySelector("[clear]").addEventListener("click", () => calc.clear());
 document.querySelector("[del]").addEventListener("click", () => calc.delete());
+
+// === Keyboard Bindings ===
+const keyMap = {
+  // Digits & dot
+  ...Object.fromEntries(
+    "0123456789.".split("").map((k) => [k, () => calc.appendNumber(k)])
+  ),
+
+  // Basic operators
+  "+": () => calc.chooseOperator("+"),
+  "-": () => calc.chooseOperator("-"),
+  "*": () => calc.chooseOperator("*"),
+  "/": () => calc.chooseOperator("/"),
+
+  // Special operations
+  r: () => calc.chooseOperator("√"), // r = root
+  "^": () => calc.chooseOperator("x²"), // ^ = square
+  "%": () => calc.chooseOperator("%"), // % = percentage
+
+  // Control keys
+  Enter: () => calc.compute(),
+  "=": () => calc.compute(),
+  Escape: () => calc.clear(),
+  Backspace: () => calc.delete(),
+};
+
+document.addEventListener("keydown", (e) => {
+  const handler = keyMap[e.key];
+  if (handler) handler();
+});
